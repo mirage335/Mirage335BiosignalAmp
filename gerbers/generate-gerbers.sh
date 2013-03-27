2da79193a7225ab238ae6fa6ba22159b20937afe
Shawn Nock <nock@nocko.se>
2013-03-27 13:07:16 -0400

    Removes unused layers in Amplifier board and updates gerber script for 4 layer parts

--- gerbers/generate-gerbers.sh
+++ gerbers/generate-gerbers.sh
@@ -2,7 +2,7 @@
 
 # Remove any old files
 find . -maxdepth 1 -name \*.zip -delete
-find . -maxdepth 1 -type d -exec rm -rf {}/* \;
+find . -maxdepth 1 -type d -and -not -name '.' -exec rm -rf {} \;
 
 # Generate Gerbers for each pcb file in the parent directory
 for pcbname in `ls .. |sed -n -e '/\.pcb/s/\.pcb$//p'`; do
@@ -17,6 +17,13 @@ find . -name \*paste\* -delete
 
 # Remove empty silk layers
 find . -name \*silk\* -size -380c -delete
+
+# Oshpark is very picky about internal layer naming (4 layer boards).
+for pcbname in `ls .. |sed -n -e '/\.pcb/s/\.pcb$//p'`; do
+    for layer in `seq 1 2`; do
+	find $pcbname -type f -name \*group$layer\* -exec mv {} $pcbname/$pcbname.G$(($layer+1))L \;
+    done
+done
 
 # Compress Gerbers
 find . -maxdepth 1 -type d -and -not -name '.' -exec zip -r {} {} \;
