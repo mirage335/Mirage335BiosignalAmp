f64c3becd42d5b363a637002576178feb45a3095
mirage335 <spamfreemirage335@gmail.com>
2013-07-03 18:37:41 +0000

    Version 1.2. Improved electrode PCBs, lower DRL cutoff frequency.

--- ActiveElectrode.cmd
+++ ActiveElectrode.cmd
@@ -1,35 +1,20 @@
 # Pin name action command file
 
+# Start of element Q1
+ChangePinName(Q1, 1, B)
+ChangePinName(Q1, 2, E)
+ChangePinName(Q1, 3, C)
+
 # Start of element Q2
-ChangePinName(Q2, 2, b)
-ChangePinName(Q2, 1, c)
-ChangePinName(Q2, 3, e)
+ChangePinName(Q2, 1, b)
+ChangePinName(Q2, 3, c)
+ChangePinName(Q2, 2, e)
 
 # Start of element CONN1
-ChangePinName(CONN1, 24, 24)
-ChangePinName(CONN1, 23, 23)
-ChangePinName(CONN1, 22, 22)
-ChangePinName(CONN1, 21, 21)
-ChangePinName(CONN1, 20, 20)
-ChangePinName(CONN1, 19, 19)
-ChangePinName(CONN1, 18, 18)
-ChangePinName(CONN1, 17, 17)
-ChangePinName(CONN1, 16, 16)
-ChangePinName(CONN1, 15, 15)
-ChangePinName(CONN1, 14, 14)
-ChangePinName(CONN1, 13, 13)
-ChangePinName(CONN1, 12, 12)
-ChangePinName(CONN1, 11, 11)
-ChangePinName(CONN1, 10, 10)
-ChangePinName(CONN1, 9, 9)
-ChangePinName(CONN1, 8, 8)
-ChangePinName(CONN1, 7, 7)
-ChangePinName(CONN1, 6, 6)
-ChangePinName(CONN1, 5, 5)
-ChangePinName(CONN1, 4, 4)
-ChangePinName(CONN1, 3, 3)
-ChangePinName(CONN1, 2, 2)
 ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 3, 3)
 
 # Start of element X1
 ChangePinName(X1, 7, shield)
@@ -47,11 +32,6 @@ ChangePinName(R1, 2, 2)
 # Start of element C9
 ChangePinName(C9, 2, 2)
 ChangePinName(C9, 1, 1)
-
-# Start of element Q1
-ChangePinName(Q1, 2, 2)
-ChangePinName(Q1, 3, 3)
-ChangePinName(Q1, 1, 1)
 
 # Start of element R3
 ChangePinName(R3, 1, 1)
@@ -84,14 +64,6 @@ ChangePinName(C5, 1, 1)
 # Start of element C6
 ChangePinName(C6, 2, 2)
 ChangePinName(C6, 1, 1)
-
-# Start of element C7
-ChangePinName(C7, 2, -)
-ChangePinName(C7, 1, +)
-
-# Start of element C8
-ChangePinName(C8, 2, -)
-ChangePinName(C8, 1, +)
 
 # Start of element R4
 ChangePinName(R4, 1, 1)
